43b8b6b55133d443f55d4b4b54c5f9844d6964b3
Benedict Owusu <beneyjoy@gmail.com>
2023-05-20 02:54:21 +0000

    added the navigation and functionalities to most buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "QPixmap"
+#include <vector>
+#include <string>
+#include "QString"
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -33,9 +37,116 @@ MainWindow::MainWindow(QWidget *parent)
 
 }
 
+// storing item selection in vector
+
+
+struct itemPrice{
+    string itemName;
+    int itemprice;
+};
+
+// declaring a global variable
+
+vector<itemPrice> v;
+int total = 0;
+
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+// Navigations
+
+
+void MainWindow::on_viewcart_clicked()
+{
+    string str;
+
+    ui->stackedWidget->setCurrentIndex(1);
+    // view selected items in a list widget
+
+    for (int i = 0; i < v.size(); i++){
+        str = v[i].itemName + "\t\t\t" + to_string(v[i].itemprice);
+        ui->addeditems->addItem(str.c_str());
+
+        total += v[i].itemprice;
+    }
+
+    str = to_string(total);
+    ui->totalPrice->setText(str.c_str());
+
+    str = to_string(total);
+    ui->totalPricePay->setText(str.c_str());
+}
+
+
+void MainWindow::on_Back_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void MainWindow::on_pay_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_confirm_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+
+void MainWindow::on_pushButton_new_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void MainWindow::on_Back_2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+//adding cart
+
+void MainWindow::on_pushButton_clicked()
+{
+    itemPrice richdadpoordad;
+    richdadpoordad.itemName = "Rich Dad Poor Dad";
+    richdadpoordad.itemprice = 25;
+
+    v.push_back(richdadpoordad);
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    itemPrice psychologyofmoney;
+    psychologyofmoney.itemName = "Psychology of Money";
+    psychologyofmoney.itemprice = 29;
+
+    v.push_back(psychologyofmoney);
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    itemPrice bookofmoney;
+    bookofmoney.itemName = "Book of Money";
+    bookofmoney.itemprice = 21;
+
+    v.push_back(bookofmoney);
+}
+
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    itemPrice speakaboutmoney;
+    speakaboutmoney.itemName = "Speak about money";
+    speakaboutmoney.itemprice = 12;
+
+    v.push_back(speakaboutmoney);
+}
 
--- mainwindow.h
+++ mainwindow.h
@@ -16,6 +16,27 @@ public:
     ~MainWindow();
 
 
+private slots:
+    void on_viewcart_clicked();
+
+    void on_Back_clicked();
+
+    void on_pay_clicked();
+
+    void on_confirm_clicked();
+
+    void on_pushButton_new_clicked();
+
+    void on_Back_2_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
